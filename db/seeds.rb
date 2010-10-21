2de2aa86c02724a2980fe82251eb4c39eb82f43b
Seth Holloway <smholloway@gmail.com>
2010-10-21 12:01:34 -0500

    updated rails version to fix security hole

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+#sensors = Sensor.create([{ :title => 'Sensor1', :description => 'Sensor1'}])
+#rules = Rule.create([{ :rule => 'if (Sensor.where("name = 'Sensor1'").first.readings.last.data > 0.to_s) then a = Actuator.where("name = 'actuator1'").first; a.command.data = 0.6; a.save end', :description => 'if (Sensor.where("name = 'Sensor1'").first.readings.last.data > 0.to_s) then a = Actuator.where("name = 'actuator1'").first; a.command.data = 0.6; a.save end'}])
