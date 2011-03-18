fcfcd2608f803dda225f0494f69a62718d658688
Seth Holloway <smholloway@gmail.com>
2011-03-18 16:50:21 -0500

    create commands in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,73 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-sensor1 = Sensor.create([{ :name => 'thermometer', :description => 'Sun SPOT reporting temperature data', :valid_values => '0..212', :manufacturer => 'Sun', :model => 'SPOT' }])
-sensor2 = Sensor.create([{ :name => 'light', :description => 'Sun SPOT reporting light levels', :valid_values => '0..100', :manufacturer => 'Sun', :model => 'SPOT' }])
-sensor3 = Sensor.create([{ :name => 'tiltometer', :description => 'Sun SPOT reporting direction in X dimension', :valid_values => '-1.58..1.58', :manufacturer => 'Sun', :model => 'SPOT' }])
+sensor1 = Sensor.create([{
+  :id => 1,
+  :name => 'thermometer',
+  :description => 'Sun SPOT reporting temperature data',
+  :valid_values => '0..212',
+  :manufacturer => 'Sun',
+  :model => 'SPOT' 
+}])
+sensor2 = Sensor.create([{ 
+  :id => 2,
+  :name => 'light', 
+  :description => 'Sun SPOT reporting light levels', 
+  :valid_values => '0..100', 
+  :manufacturer => 'Sun', 
+  :model => 'SPOT' 
+}])
+sensor3 = Sensor.create([{ 
+  :id => 3,
+  :name => 'tiltometer', 
+  :description => 'Sun SPOT reporting direction in X dimension', 
+  :valid_values => '-1.58..1.58', 
+  :manufacturer => 'Sun', 
+  :model => 'SPOT' 
+}])
 
-actuator1 = Actuator.create([{ :name => 'thermostat', :description => 'Sun SPOT device representing a thermostat LEDs display the set temperature in binary', :valid_values => '0..255', :manufacturer => 'Sun', :model => 'SPOT' }])
-actuator1.command = Command.new
-actuator2 = Actuator.create([{ :name => 'alarm', :description => 'Sun SPOT device representing an alarm', :valid_values => '0..255', :manufacturer => 'Sun', :model => 'SPOT' }])
-actuator2.command = Command.new
-actuator3 = Actuator.create([{ :name => 'fan', :description => 'Sun SPOT device representing a fan', :valid_values => 'off,low,medium,high', :manufacturer => 'Sun', :model => 'SPOT' }])
-actuator3.command = Command.new
+actuator1 = Actuator.create([{ 
+  :id => 1,
+  :name => 'thermostat', 
+  :description => 'Sun SPOT device representing a thermostat LEDs display the set temperature in binary', 
+  :valid_values => '0..255', 
+  :manufacturer => 'Sun', 
+  :model => 'SPOT' 
+}])
+actuator2 = Actuator.create([{ 
+  :id => 2,
+  :name => 'alarm', 
+  :description => 'Sun SPOT device representing an alarm', 
+  :valid_values => '0..255', 
+  :manufacturer => 'Sun', 
+  :model => 'SPOT' 
+}])
+actuator3 = Actuator.create([{ 
+  :id => 3,
+  :name => 'fan', 
+  :description => 'Sun SPOT device representing a fan', 
+  :valid_values => 'off,low,medium,high', 
+  :manufacturer => 'Sun', 
+  :model => 'SPOT' 
+}])
 
-rule1 = Rule.create([{ :rule => 'if ((Sensor.find(3).readings.first.data < 0.to_s)) then a = Actuator.find(3).command; a.data=\"high\"; a.save; end', :description => 'if the x sensor is negative, turn the fan to high' }])
-rule2 = Rule.create([{ :rule => 'if ((Sensor.find(1).readings.first.data > 75.to_s)) then a = Actuator.find(1).command; a.data=72.to_s; a.save; end', :description => 'when its hot, turn on the thermostat' }])
+command1 = Command.create([{ 
+  :actuator_id => 1
+}])
+command2 = Command.create([{ 
+  :actuator_id => 2
+}])
+command3 = Command.create([{ 
+  :actuator_id => 3
+}])
+
+rule1 = Rule.create([{ 
+  :id => 1,
+  :rule => 'if ((Sensor.find(3).readings.first.data < 0.to_s)) then a = Actuator.find(3).command; a.data=\"high\"; a.save; end', 
+  :description => 'if the x sensor is negative, turn the fan to high' 
+}])
+rule2 = Rule.create([{ 
+  :id => 2,
+  :rule => 'if ((Sensor.find(1).readings.first.data > 75.to_s)) then a = Actuator.find(1).command; a.data=72.to_s; a.save; end', 
+  :description => 'when its hot, turn on the thermostat' 
+}])
