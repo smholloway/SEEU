b5de2f78e4ae14c4d50c301fcb500be6c65f70bb
Seth Holloway <smholloway@gmail.com>
2011-03-19 13:11:46 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,7 +74,7 @@ rule1 = Rule.create([{
 rule2 = Rule.create([{ 
   :id => 2,
   :rule => 'if ((Sensor.find(3).readings.first.data >= 0.to_s)) then a = Actuator.find(3).command; a.data = "low"; a.save; end', 
-  :description => 'if the tiltometer is negative, turn the fan to high' 
+  :description => 'if the tiltometer is positive, turn the fan to low' 
 }])
 rule3 = Rule.create([{ 
   :id => 3,
